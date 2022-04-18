61df2745052f8ec994ff59b6d00858a330202fe6
takanashi66 <takanashi66@gmail.com>
2022-04-18 11:43:06 +0900

    コンフィグのスタイル

--- resources/views/config/index.blade.php
+++ resources/views/config/index.blade.php
@@ -5,21 +5,23 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="post" action="{{ route('config.public.update') }}">
-                        @csrf
-                        <label><input type="radio" name="is_public" value="1" {{ old('is_public', ($user) ? $user->is_public : '')===1 ? 'checked':'' }}>公開する</label>
-                        <label><input type="radio" name="is_public" value="0" {{ old('is_public', ($user) ? $user->is_public : '')===0 ? 'checked':'' }}>公開しない</label>
-                        <button type="submit">送信</button>
-                    </form>
-                    @if ($errors->has('is_public'))
-                        <p class="error_text"><span>{{$errors->first('is_public')}}</span></p>
-                    @endif
-                </div>
-            </div>
+    <div class="page">
+        
+        <div class="pageitem">
+            <h3>アカウントの公開・非公開</h3>
+            <form method="post" action="{{ route('config.public.update') }}">
+                @csrf
+                <label><input type="radio" name="is_public" value="1" checked {{ old('is_public', ($user) ? $user->is_public : '')===1 ? 'checked':'' }}>公開</label>
+                <label><input type="radio" name="is_public" value="0" {{ old('is_public', ($user) ? $user->is_public : '')===0 ? 'checked':'' }}>非公開</label>
+                <button cla type="submit">保存</button>
+            </form>
+            @if ($errors->has('is_public'))
+                <p class="error_text"><span>{{$errors->first('is_public')}}</span></p>
+            @endif
         </div>
+        
+        
     </div>
+    
+    
 </x-app-layout>
