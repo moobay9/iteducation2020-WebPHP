878504d7fed5c4e7355e189ca99cb78a13b3943b
takanashi66 <takanashi66@gmail.com>
2022-04-18 10:30:28 +0900

    breeze手動マージ

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,39 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('tailwindcss'),
     require('autoprefixer'),
 ]);
+
+mix.js('resources/assets/js/script.js', 'public/common/js')
+    .sass('resources/assets/scss/style.scss', 'public/common/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('autoprefixer')({
+                grid: true
+            }),
+            require('postcss-sort-media-queries')()
+        ]
+    })
+    .sourceMaps()
+
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    })
+} else {
+    mix.version();
+}
+
+mix.browserSync({
+    server: false,
+    proxy: 'http://localhost:20080',
+    port: 3300,
+    files: [
+        'public/**/*.html',
+        'public/**/*.php',
+        'public/common/js/*.js',
+        'public/common/css/*.css',
+    ],
+    ui: {
+        port: 3301
+    }
+})
