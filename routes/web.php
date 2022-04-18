09f1f8793067230a06bcd8eb52172aaa3a93d38f
takanashi66 <takanashi66@gmail.com>
2022-04-18 11:55:00 +0900

    ユーザーページ

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/', function () {
     return redirect('/top');
 });
 
+// ユーザーページ
+Route::get('/user', function () {
+    return view('user.page');
+});
+
 Route::get('/top', function () {
     return view('dashboard');
 })->name('top');
